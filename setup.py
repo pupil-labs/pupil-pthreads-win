3f12dd6df8f1c2be3033033060938008d44e0307
Patrick Faion <pfa@pupil-labs.com>
2019-08-27 09:11:55 +0200

    Apply black formatter

--- setup.py
+++ setup.py
@@ -6,18 +6,17 @@ package = "pupil_pthreads_win"
 
 package_data = [
     str(match.resolve())
-    for match in (Path() / package_dir / package / "data").rglob('*')
+    for match in (Path() / package_dir / package / "data").rglob("*")
     if match.is_file()
 ]
 
 setup(
     name="pupil-pthreads-win",
-    version='0.dev0',
     # url="https://github.com/pupil-labs/github-2-clickup",
+    version="0.dev0",
     license="MIT License",
     author="Pupil Labs GmbH",
     author_email="pypi@pupil-labs.com",
-
     packages=[package],
     package_dir={"": package_dir},
     package_data={package: package_data},
