660a8f5d6a70ef31ee1ef63cc6a4544839571e63
Patrick Faion <pfa@pupil-labs.com>
2019-09-18 15:13:05 +0200

    Read version automatically from __init__.py

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
-from setuptools import setup
+import codecs
+import os
+import re
 from pathlib import Path
+
+from setuptools import setup
 
 with open("README.md") as f:
     readme_text = f.read()
@@ -13,12 +17,25 @@ package_data = [
     if match.is_file()
 ]
 
+here = Path(__file__).parent
+
+
+def read_version():
+    path = here / "src" / "pupil_pthreads_win" / "__init__.py"
+    with codecs.open(path, "r") as fp:
+        version_file = fp.read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
 setup(
     name="pupil-pthreads-win",
     description="A precompiled version of pthreads-win.",
     long_description=readme_text,
     long_description_content_type="text/markdown",
-    version="1",
+    version=read_version(),
     url="https://github.com/pupil-labs/pupil-pthreads-win",
     license="MIT License",
     author="Pupil Labs GmbH",
